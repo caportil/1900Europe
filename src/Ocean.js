de763ca9aa855f8680e898fb3f69d00ba7a3e52a
caportil <caportil@gmail.com>
2017-02-16 23:39:42 -0800

    Begin combining custom area maps + image renders into unified components

--- src/Ocean.js
+++ src/Ocean.js
@@ -24,14 +24,16 @@ class Ocean extends Component {
 	console.log('Running a render in ' + self.props.class + '...' + 'occupiedOcean tiles are currently: ' + self.props.occupiedOcean)
 	return (
 
-		<img src={`${self.props.src}.png`}
+		<img src={`${self.props.src2}.png`}
 		  class={self.props.class}
 		  alt="" 
 		  useMap="#combinedMap"
 		  style=
 		  	{{position: 'absolute', 
-	    	  left: `${Number(self.props.left) + 100}px`,
-	    	  top: `${Number(self.props.top) + 200}px`,
+	    	  // left: `${Number(self.props.left) + 100}px`,
+	    	  // top: `${Number(self.props.top) + 200}px`,
+	    	  left: '100px',
+	    	  top: '200px',
 	    	  filter: `hue-rotate(${self.toggleHue()}deg) opacity(45%) opacity(70%)`
 			}} 
 		/>
