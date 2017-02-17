03f566d476c99cf73c8bf50a49c46add05ce6e5e
caportil <caportil@gmail.com>
2017-02-17 00:12:40 -0800

    Finish combining Ocean tiles with Ocean Area maps

--- src/Ocean.js
+++ src/Ocean.js
@@ -23,21 +23,36 @@ class Ocean extends Component {
 	let self = this;
 	console.log('Running a render in ' + self.props.class + '...' + 'occupiedOcean tiles are currently: ' + self.props.occupiedOcean)
 	return (
+		<div>
 
-		<img src={`${self.props.src2}.png`}
-		  class={self.props.class}
-		  alt="" 
-		  useMap="#combinedMap"
-		  style=
-		  	{{position: 'absolute', 
-	    	  // left: `${Number(self.props.left) + 100}px`,
-	    	  // top: `${Number(self.props.top) + 200}px`,
-	    	  left: '100px',
-	    	  top: '200px',
-	    	  filter: `hue-rotate(${self.toggleHue()}deg) opacity(45%) opacity(70%)`
-			}} 
-		/>
+			<img src={`${self.props.src}.png`}
+			  class={self.props.class}
+			  alt="" 
+			  useMap={`#${self.props.class}Map`}
+			  style=
+			  	{{position: 'absolute', 
+		    	  left: `${Number(self.props.left) + 100}px`,
+		    	  top: `${Number(self.props.top) + 200}px`,
+		    	  // left: '100px',
+		    	  // top: '200px',
+		    	  filter: `hue-rotate(${self.toggleHue()}deg) opacity(45%) opacity(70%)`
+				}} 
+			/>
 
+			<map name={`${self.props.class}Map`}>
+				<area 
+					class={self.props.class} 
+					alt="" 
+					title="" 
+					nohref="nohref" 
+					shape="poly" 
+					coords={self.props.coords2}
+					style={{cursor: 'default'}}
+					onClick={() => self.props.toggleOccupiedOcean(self.props.class) }
+				/>
+ 			</map>
+
+		</div>
 		)
 	}
 }
