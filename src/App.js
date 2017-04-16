60196f67afdabcbdbc5be78be587a4995c20ebc8
caportil <caportil@gmail.com>
2017-04-16 00:11:16 -0700

    Add units objects and locaitons for remaining countries

--- src/App.js
+++ src/App.js
@@ -23,7 +23,31 @@ class App extends Component {
 				england: {
 					infantry: ['Liverpool'],
 					navy: ['Edinburgh', 'London']
-				}
+				},
+        germany: {
+          infantry: ['Berlin', 'Munich'],
+          navy: ['Kiel']
+        },
+        france: {
+          infantry: ['Paris', 'Marseilles'],
+          navy: ['Brest']
+        },
+        italy: {
+          infantry: ['Venice', 'Rome'],
+          navy: ['Naples']
+        },
+        austria: {
+          infantry: ['Vienna', 'Budapest'],
+          navy: ['Trieste']
+        },
+        russia: {
+          infantry: ['Moscow', 'Warsaw'],
+          navy: ['StPetersburg']
+        },
+        ottomans: {
+          infantry: ['Constantinople', 'Smyrna'],
+          navy: ['Ankara']
+        },
 			},
 			latestChange: '',
 			newOwner: false,
