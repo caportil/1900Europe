464c0527a8230a34b3f5cea8200a3618317d344b
caportil <caportil@gmail.com>
2017-04-19 17:53:49 -0700

    Pass main territories logic from action to reducer to properly access store

--- src/actions/TerritoriesActions.js
+++ src/actions/TerritoriesActions.js
@@ -1,32 +1,55 @@
-export default function transferOwnership(props, tile, fromm, to) {
-  // let self = this;
-  // // console.log('self.state is:', self.state, 'and self.state[from] is:', self.state[from])
-  // let fromCountry = self.state[fromm];
-  // let toCountry = self.state[to];
-  // // console.log('transferOwnership invoked! fromCountry is:', fromCountry)
-  // let fromIndex = fromCountry.indexOf(tile);
-  // let toIndex = toCountry.indexOf(tile);
-  // let newStateObject = {};
-  // fromCountry = fromCountry.slice(0, fromIndex).concat(fromCountry.slice(fromIndex + 1));
-  // toCountry.push(tile);
-  // newStateObject[fromm] = fromCountry;
-  // newStateObject[to] = toCountry;
-  // newStateObject['latestChange'] = tile;
-  // self.setState(newStateObject);
-
-  return function(dispatch) {
-    let state = store.getState().territories;
-    let fromCountry = state[fromm];
-    let toCountry = state[to];
-    let fromIndex = fromCountry.indexOf(tile);
-    let toIndex = toCountry.indexOf(tile);
-    let newStateObject = {type: 'CHANGE_TILE'};
-    fromCountry = fromCountry.slice(0, fromIndex).concat(fromCountry.slice(fromIndex + 1));
-    toCountry.push(tile);
-    newStateObject[fromm] = fromCountry;
-    newStateObject[to] = toCountry;
-    newStateObject['latestChange'] = tile;
+export default {
+  transferOwnership: (tile) => {
+    console.log('Running transferOwnership... tile parameter is:', tile);
+    // let self = this;
+    // // console.log('state is:', state, 'and state[from] is:', state[from])
+    // let fromCountry = state[fromm];
+    // let toCountry = state[to];
+    // // console.log('transferOwnership invoked! fromCountry is:', fromCountry)
+    // let fromIndex = fromCountry.indexOf(tile);
+    // let toIndex = toCountry.indexOf(tile);
+    // let newStateObject = {};
+    // fromCountry = fromCountry.slice(0, fromIndex).concat(fromCountry.slice(fromIndex + 1));
+    // toCountry.push(tile);
+    // newStateObject[fromm] = fromCountry;
+    // newStateObject[to] = toCountry;
+    // newStateObject['latestChange'] = tile;
     // self.setState(newStateObject);
-    dispatch(newStateObject);
+
+    return function(dispatch) {
+      console.log('Running callback function(dispatch) within transferOwnership...');
+      // let state = store.getState().territories;
+      // console.log('After running store.getState(), state is now:', state);
+      // let fromm = findOwnership(tile);
+      // let to = state.newOwner;
+      // let fromCountry = state[fromm];
+      // let toCountry = state[to];
+      // let fromIndex = fromCountry.indexOf(tile);
+      // let toIndex = toCountry.indexOf(tile);
+      // let newStateObject = {type: 'CHANGE_TILE', payload: {}};
+      // fromCountry = fromCountry.slice(0, fromIndex).concat(fromCountry.slice(fromIndex + 1));
+      // toCountry.push(tile);
+      // newStateObject.payload[fromm] = fromCountry;
+      // newStateObject.payload[to] = toCountry;
+      // newStateObject.payload.latestChange = tile;
+      // self.setState(newStateObject);
+      // console.log('Within TerritoriesActions.transferOwnership, newStateObject is:', newStateObject);
+
+
+      // dispatch(newStateObject);
+      dispatch({
+        type: 'CHANGE_TILE',
+        payload: tile
+      })
+    }
+  },
+  loadNewOwner: (country) => {
+    console.log('Running loadNewOwner... country parameter is:', country);
+    return function(dispatch) {
+      dispatch({
+        type: 'LOAD_NEW_OWNER',
+        payload: {newOwner: country}
+      })
+    }
   }
 }
--- src/reducers/Reducers.js
+++ src/reducers/Reducers.js
@@ -3,13 +3,57 @@ import startingTerritories from './../components/Territories.js';
 import startingUnits from './../components/Units.js';
 
 const territoriesReducer = (state = startingTerritories, action) => {
-  if (action.type === 'ADD_LIST') {
-    state = state.concat([action.payload])
+  // if (action.type === 'ADD_LIST') {
+  //   newState.concat([action.payload])
+  // }
+  const findOwnership = (state, tile) => {
+    // let self = this;
+    if (state.england.indexOf(tile) > -1) {
+      console.log('england found!')
+      return 'england';
+    } else if (state.germany.indexOf(tile) > -1) {
+      console.log('germany found!')
+      return 'germany';
+    } else if (state.france.indexOf(tile) > -1) {
+      return 'france';
+    } else if (state.italy.indexOf(tile) > -1) {
+      return 'italy';
+    } else if (state.austria.indexOf(tile) > -1) {
+      return 'austria';
+    } else if (state.russia.indexOf(tile) > -1) {
+      return 'russia';
+    } else if (state.ottomans.indexOf(tile) > -1) {
+      return 'ottomans';
+    } else {
+      console.log('Nobody owns this tile!')
+      return 'neutral';
+    }
   }
-  if (action.type === 'CHANGE_OWNER') {
 
+  console.log('In territoriesReducer, action is:', action);
+  if (action.type === 'LOAD_NEW_OWNER') {
+    var newState = Object.assign({}, state, action.payload);
+
+  } else if (action.type === 'CHANGE_TILE') {
+    let tile = action.payload;
+    var newState = Object.assign({}, state);
+    let fromm = findOwnership(newState, tile);
+    console.log('fromm is now:', fromm);
+    let to = newState.newOwner;
+    let fromCountry = newState[fromm];
+    let toCountry = newState[to];
+    let fromIndex = fromCountry.indexOf(tile);
+    let toIndex = toCountry.indexOf(tile);
+    // let newStateObject = {type: 'CHANGE_TILE', payload: {}};
+    fromCountry = fromCountry.slice(0, fromIndex).concat(fromCountry.slice(fromIndex + 1));
+    toCountry.push(tile);
+    newState[fromm] = fromCountry;
+    newState[to] = toCountry;
+    newState.latestChange = tile;
+  } else {
+    var newState = state;
   }
-  return state;
+  return newState;
 };
 
 const unitsReducer = (state = startingUnits, action) => {
