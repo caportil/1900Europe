55307efedfb73391e3cdc2b403fe995a45d83efd
caportil <caportil@gmail.com>
2017-04-24 23:50:18 -0700

    Modify ocean component for selected tile highlighting

--- src/components/Ocean.js
+++ src/components/Ocean.js
@@ -8,17 +8,17 @@ class Ocean extends Component {
 		}
 	}
 
-	toggleHue() {
+	toggleHighlight() {
 		let self = this;
 		return self.props.occupiedOcean.indexOf(self.props.class) > -1 ? 180 : 240;
 	}
 
 	render() {
 		let self = this;
-		// console.log('Running a render in ' + self.props.class + '...' + 'occupiedOcean tiles are currently: ' + self.props.occupiedOcean)
 		return (
 			<path 
 				d={self.props.path}
+				// style={{fill: self.toggleHighlight()}}
 				style={{fill: `#80ccff`}}
 				onClick={() => console.log(`Clicked on ${self.props.class}!`)}
 			/>
